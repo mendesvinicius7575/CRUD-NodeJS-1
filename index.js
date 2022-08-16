4dc3b1295b6e7f334d97849d2949866a5fed663c
Vinicius Emanoel Mendes <mendes.vinicius7575@gmail.com>
2022-08-16 15:36:49 -0300

    alterando Valores que vem de fora

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 
 
+const ola =  {n: "roebrson"}
+    
 
+async function enviar(ola2){
 
+    let name = "anderson"
 
-(async () =>{
     
     const db = require("./db");
 
-    //console.log("INSERT")
-    //const inserir = await db.insertCustomer({nome: "Maria", idade: 18, uf: "SP"})
+    console.log("INSERT")
+    await db.insertCustomer({nome: ola2.n, idade: 18, uf: "SP"})
 
     console.log('UPDATE')
-    const upd = await db.updateCustomer(4, {nome: "josef", idade: 55, uf: "RG"})
+    await db.updateCustomer(6, {id: 5, nome: "josef", idade: 55, uf: "RG"})
 
-    await db.deleteCustomer(5)
+    //await db.deleteCustomer(5)
 
     console.log("SELECT")
     const clientes = await db.selectCustomer();
     console.log(clientes)
 
-})();
+    console.log(name)
+
+};
+
+enviar(ola)
+
